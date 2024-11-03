41f0a4947f4832b8e98cb42805d429f1a48e8345
Ritika Mishra <mishraritika579@gmail.com>
2024-11-03 23:13:10 +0530

    initial commit

---
+++ server/controllers/blogController.js
@@ -0,0 +1,51 @@
+const Blog = require("../models/blogModel");
+
+exports.getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    res.json(blog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.createBlog = async (req, res) => {
+  const { title, imageUrl, content, author } = req.body;
+  const newBlog = new Blog({ title, imageUrl, content, author });
+
+  try {
+    const savedBlog = await newBlog.save();
+    res.status(201).json(savedBlog);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.updateBlog = async (req, res) => {
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedBlog);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.json({ message: "Blog deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
